52bebce34ab51afcd914a8c3c91218edc1ade396
Jason Thomas <jason@lithiumfox.com>
2019-02-13 17:52:28 -0500

    Use enumerate and next to capture first element

--- parquet2bigquery/lib.py
+++ parquet2bigquery/lib.py
@@ -114,13 +114,8 @@ def _get_object_key_metadata(object_key):
 
     split_key = object_key.split('/')
 
-    p_elems_idx = []
-
-    for elem in split_key:
-        if '=' in elem:
-            p_elems_idx.append(split_key.index(elem))
-
-    first_part_idx = min(p_elems_idx)
+    first_part_idx = next([index for index, elem in enumerate(split_key)
+                          if '=' in elem])
 
     table_version = split_key[first_part_idx - 1]
     table_name = split_key[first_part_idx - 2]
